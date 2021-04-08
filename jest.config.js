7801bd2d46c99abfbe78fe9113ef243d205dd379
LariCode <larissa.santosazevedo@hotmail.com>
2021-04-07 23:14:43 -0300

    configure jest

--- jest.config.js
+++ jest.config.js
@@ -5,29 +5,12 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["components/**/*.js", "pages/**/*.js"],
   coverageReporters: ["lcov", "text"],
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
-  roots: ["<rootDir>/src"],
-
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
-
-  // Runs special logic, such as cleaning up components
-  // when using React Testing Library and adds special
-  // extended assertions to Jest
-  setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect",
-  ],
-
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-
-  // Module file extensions for importing
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+  },
 };
